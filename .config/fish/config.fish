236dfa818cc87290022d89c4abc52a5bbb0a1af5
Jimmy Vogel <james_vogel@waters.com>
2018-11-02 10:53:17 -0400

    Fix fish startup scripts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,13 @@
 # Set environment variables
+set -gx PATH $HOME/.local/bin $PATH
+set -gx SHELL /bin/fish
+set -gx GNUTERM sixel
+
+# Create aliases
 alias config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-set -gx PATH "$HOME/.local/bin" $PATH
-set -gx SHELL "/bin/fish"
 
 # Path to Oh My Fish install.
-set -gx OMF_PATH "/cygdrive/c/Users/usjvog/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/cygdrive/c/Users/usjvog/.config/omf"
+set -gx OMF_PATH $HOME/.local/share/omf
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
--- .config/omf/init.fish
+++
@@ -1,5 +0,0 @@
-# Used for gnuplot
-set -xg GNUTERM sixel
-
-# Alias for dotfile repo
-alias config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
